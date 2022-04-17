f5dab64782650b05a00f0ad1fbe9f5bb0fe3a0d6
saddamarbaa <saddam@rushowl.sg>
2022-04-17 18:05:37 +0700

    feat: add theme switch Dark/Light Mode

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,20 +1,49 @@
-import React from 'react'
-import { MoonIcon } from '@heroicons/react/outline'
+import { useEffect, useState } from 'react';
+import { MoonIcon, SunIcon } from '@heroicons/react/solid';
+import Link from 'next/link';
+import { useTheme } from 'next-themes';
 
-const Header = () => {
-	return (
-		<header className="bg-white shadow sticky top-0 z-50">
-			<div className="w-full max-w-[82rem] mx-auto flex items-center justify-between p-4 py-6 ">
-				<div className="font-bold cursor-pointer text-[1.2rem]">
-					Where in the world?
-				</div>
-				<div className="flex items-center space-x-2">
-					<MoonIcon className="h-5 cursor-pointer mt-1" />
-					<p className="cursor-pointer text-gray-700">Dark Mode</p>
-				</div>
-			</div>
-		</header>
-	)
+export default function Header() {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const renderThemeChanger = () => {
+    if (!mounted) return null;
+
+    const currentTheme = theme === 'system' ? systemTheme : theme;
+
+    if (currentTheme === 'dark') {
+      return (
+        <SunIcon
+          className="h-10 w-10 text-yellow-500 "
+          role="button"
+          onClick={() => setTheme('light')}
+        />
+      );
+    }
+    return (
+      <MoonIcon
+        className="h-10 w-10 text-gray-900 "
+        role="button"
+        onClick={() => setTheme('dark')}
+      />
+    );
+  };
+
+  return (
+    <header className="sticky top-0 z-50 bg-white shadow dark:border-gray-700 dark:bg-[#212E37] dark:shadow-lg ">
+      <div className="mx-auto flex w-full max-w-[82rem] items-center justify-between p-4 py-6 ">
+        <div className="cursor-pointer whitespace-nowrap font-sans text-[1.2rem] font-bold   tracking-tight text-slate-900 dark:text-white">
+          <Link href="/">
+            <a>Where in the world?</a>
+          </Link>{' '}
+        </div>
+        {renderThemeChanger()}
+      </div>
+    </header>
+  );
 }
-
-export default Header
